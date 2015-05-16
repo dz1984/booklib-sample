56e3ab1dcd9b3d6a4a804de2971d4b37e51131f1
Donald Zhan <donald.zhan1984@gmail.com>
2015-05-16 09:55:26 +0800

    translate to zh-TW

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,17 +9,17 @@
         <div id="books">
             <form id="addBook" action="#">
                 <div>
-                    <label for="coverImage">CoverImage: </label>
+                    <label for="coverImage">封面圖檔: </label>
                     <input id="coverImage" type="file" />
-                    <label for="title">Title: </label>
+                    <label for="title">書名: </label>
                     <input id="title" type="text" />
-                    <label for="author">Author: </label>
+                    <label for="author">作者: </label>
                     <input id="author" type="text" />
-                    <label for="releaseDate">Release date: </label>
+                    <label for="releaseDate">出版日期: </label>
                     <input id="releaseDate" type="text" />
-                    <label for="keywords">Keywords: </label>
+                    <label for="keywords">關鍵字: </label>
                     <input id="keywords" type="text" />
-                    <button id="add">Add</button>
+                    <button id="add">增加</button>
                 </div>
             </form>
         </div>
